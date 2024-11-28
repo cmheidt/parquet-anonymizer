39ab7b36b2a0ea6ca105086e78d2d4acd3568d5b
Chris Heidt <christian.heidt@med.uni-heidelberg.de>
2024-11-28 14:49:32 +0000

    Implement anonymizer code for dfs, csvs, xslx and pqt

--- parquet_anonymizer/anonymizer.py
+++ parquet_anonymizer/anonymizer.py
@@ -1,5 +1,54 @@
 import polars as pl
 
+from parquet_anonymizer.config import Config
+from parquet_anonymizer.field_types.field_type_factory import FieldTypeFactory
+from parquet_anonymizer.user.user_callback import UserCallback
 
-def anonymize_dataframe(df: pl.DataFrame, config: dict, seed: str) -> pl.DataFrame:
-    
+
+def anonymize_dataframe(
+    config: Config, df: pl.DataFrame, user_callback: UserCallback = None
+) -> pl.DataFrame:
+    for column_name in config.columns_to_anonymize:
+        if column_name not in df.columns:
+            raise ValueError(f"{column_name} not found in dataframe.")
+
+        type_config_dict = config.columns_to_anonymize[column_name]
+        field_type = FieldTypeFactory.get_type(type_config_dict)
+
+        if field_type is not None:
+            df = df.with_column(
+                pl.when(pl.col(column_name).is_not_null() & (pl.col(column_name) != ""))
+                .then(
+                    pl.col(column_name).apply(
+                        lambda x: field_type.generate_obfuscated_value(
+                            config.secret_key, x, user_callback
+                        )
+                    )
+                )
+                .otherwise(pl.col(column_name))
+                .alias(column_name)
+            )
+
+    return df
+
+
+def anonymize_csv(config: Config, in_filename: str, out_filename: str, user_callback: UserCallback):
+    df = pl.read_csv(in_filename, separator=config.delimiter)
+    df = anonymize_dataframe(config, df, user_callback)
+    df.write_csv(out_filename, separator=config.delimiter)
+
+
+def anonymize_parquet(
+    config: Config, in_filename: str, out_filename: str, user_callback: UserCallback
+):
+    df = pl.read_parquet(in_filename)
+    df = anonymize_dataframe(config, df, user_callback)
+    df.write_parquet(out_filename)
+
+
+def anonymize_xslx(
+    config: Config, in_filename: str, out_filename: str, user_callback: UserCallback
+):
+    df = pl.read_excel(in_filename)
+    df = anonymize_dataframe(config, df, user_callback)
+    df.write_excel(out_filename)
